99fd6b1b0949d868c93847a7f5e4d512d134c963
SuzuOkumura <44101bwdive@gmail.com>
2020-03-19 14:11:21 +0000

    Laravel12 Practice

--- routes/web.php
+++ routes/web.php
@@ -38,4 +38,6 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 Route::group(['prefix' => 'admin'], function () {
     Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
+    Route::get('profile/create', 'Admin\ProfileController@add')->middleware('auth');
+    Route::get('profile/edit', 'Admin\ProfileController@edit')->middleware('auth');
 });
