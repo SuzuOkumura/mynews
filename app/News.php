02847002c48a62cd79018ca6b7198b1266967ac9
SuzuOkumura <44101bwdive@gmail.com>
2020-04-14 14:39:27 +0000

    Laravel17_1

--- app/News.php
+++ app/News.php
@@ -12,4 +12,10 @@ class News extends Model
       'title' => 'required',
       'body' => 'required',
       );
+      
+    //News Model に関連付けを行う
+    public function histories()
+    {
+        return $this->hasMany('App\History');
+    }
 }
